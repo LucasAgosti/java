2a1f1110520ab2a024bca89f4777a35fb0076127
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2018-08-05 11:57:05 -0300

    GAME

---
+++ Random.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Random;
+
+class JogoAleatorio{
+	public static void main(String[] args){
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Digite um int entre 0 e 10");
+	
+		int entrada = teclado.nextInt();
+		int random = new Random().nextInt(10) + 1;
+		
+		if(entrada == random){
+			System.out.println("\n*-----------------------*");
+			System.out.println("         You won!");
+			System.out.println("*-----------------------*");
+		}else{
+			System.out.println("*-----------------------*");
+			System.out.println("        You lose!");
+			System.out.println("*-----------------------*");
+		}
+		
+		System.out.println("\nThanks for playing!\n\n");
+	}
+}
