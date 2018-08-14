1f0848beb7a9a07cf0a643f703d616e61e58d657
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2018-08-14 17:53:47 -0300

    Java

---
+++ training/Classes/Pessoa.java
@@ -0,0 +1,47 @@
+public class Pessoa{
+
+	private String nome;
+	private String endereco;
+	private String dataNascimento;
+	private String nomePai;
+	private String nomeMae;
+	
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+	public String getNome(){
+		return nome;
+	}
+	
+	public void setEndereco(String endereco){
+		this.endereco = endereco;
+	}
+	public String getEndereco(){
+		return endereco;
+	}
+	
+	public void setDataNascimento(String dataNascimento){
+		this.dataNascimento = dataNascimento;
+	}
+	public String getDataNascimento(){
+		return dataNascimento;
+	}
+	
+	public void setNomePai(String nomePai){
+		this.nomePai = nomePai;
+	}
+	public String getNomePai(){
+		return nomePai;
+	}
+			
+	public void setNomeMae(String nomeMae){
+		this.nomeMae = nomeMae;
+	}
+	public String getNomeMae(){
+		return nomeMae;
+	
+	//CRIANDO FUNÇÃO PARA CRIAR UM DISPLAY
+	
+	
+	}
+}
