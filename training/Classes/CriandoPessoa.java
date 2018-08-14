b6e89cc002a59dee2deb147d444add52e55ca210
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2018-08-14 17:54:36 -0300

    Java

---
+++ training/Classes/CriandoPessoa.java
@@ -0,0 +1,31 @@
+import static java.lang.System.out;
+import java.util.Scanner;
+
+class CriandoPessoa{
+	public static void main(String[] args){
+
+		Pessoa pessoa1 = new Pessoa();
+		Scanner n1 = new Scanner(System.in);
+				
+		out.println("Crie sua pessoa citando seu/sua : nome - endereço - data de nascimento - nome do pai e nome da mãe");
+		
+		String nome1 = n1.nextLine();
+		String endereco1 = n1.nextLine();
+		String dataNascimento1 = n1.nextLine();
+		String pai1 = n1.nextLine();
+		String mae1 = n1.nextLine();
+		
+		pessoa1.setNome(nome1);
+		pessoa1.setEndereco(endereco1);	
+		pessoa1.setDataNascimento(dataNascimento1);
+		pessoa1.setNomePai(pai1);
+		pessoa1.setNomeMae(mae1);
+		
+		out.println("Nome: " + pessoa1.getNome());
+		out.println("Endereço: " + pessoa1.getEndereco());
+		out.println("Data de nascimento: " + pessoa1.getDataNascimento());
+		out.println("Nome do pai: " + pessoa1.getNomePai());
+		out.println("Nome da mãe: " + pessoa1.getNomeMae());
+		
+	}
+}
