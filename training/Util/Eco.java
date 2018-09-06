b7d630bbf4c5688a3e9f764a9db1ac531ab492b4
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2018-09-06 11:12:42 -0300

    Java

---
+++ training/Util/Eco.java
@@ -0,0 +1,10 @@
+	//PROGRAMA PARA LER DIGITOS DO TERMINAL
+class Eco{
+	public static void main(String[] args){
+	
+		for(int i = 0; i < args.length; i++)
+			System.out.println(args[i] + " ");
+			
+		System.out.println("\n");
+	}
+}
