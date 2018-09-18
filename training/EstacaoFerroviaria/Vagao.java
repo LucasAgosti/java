3546fdec64a312f2862a86ad33a9f73ae03b4707
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2018-09-18 11:51:43 -0300

    Java

--- training/EstacaoFerroviaria/Vagao.java
+++ training/EstacaoFerroviaria/Vagao.java
@@ -8,6 +8,7 @@ public class Vagao{
 	private double compEntreTesteiras;
 	private double compDosEngates;
 	
+	//MÉTODOS SET E GET
 	public void setNumSerie(String numeroDeSerie){
 		this.numeroDeSerie = numeroDeSerie;
 	}
@@ -44,6 +45,11 @@ public class Vagao{
 	public void setCompDosEngates(double compDosEngates){
 		this.compDosEngates = compDosEngates;
 	}
+	public double getCompDosEngates(){
+		return compDosEngates;
+	}
+	
+	//MÉTODOS DO VAGÃO
 	
 	public void carregar(double carga){
 		if((this.capacidadeAtual - carga) < 0){
@@ -51,7 +57,6 @@ public class Vagao{
 		}else{
 			this.capacidadeAtual -= carga;
 		}
-
 	}
 	public void descarregar(){
 		this.capacidadeAtual = this.capacidadeTotal;
@@ -62,6 +67,17 @@ public class Vagao{
 		if((this.capacidadeAtual + carga) > this.capacidadeTotal){
 			System.out.println("Carga desejado ultrapassa a carga limite!\nO vagão foi descarregado!")
 		}
+	}
+	
+	//CONSTRUTORES
+	
+	public Vagao(String numeroDeSerie, String tipo, double capacidadeCargaTotal, double capacidadeCargaAtual, double compEntreTesteiras, double compDosEngates){
+		this.numeroDeSerie = numeroDeSerie;
+		this.tipo = tipo;
+		this.capacidadeCargaTotal = capacidadeCargaTotal;
+		this.capacidadeCargaAtual = capacidadeCargaAtual;
+		this.compEntreTesteiras = compEntreTesteiras;
+		this.compDosEngates = compDosEngates;
 	}
 	
 	public String toString(){
