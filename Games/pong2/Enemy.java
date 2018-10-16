6ae3cd3bc0434ff656054d40cb5fdff8007a47e7
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2018-10-16 11:03:21 -0300

    Java

--- Games/pong2/Enemy.java
+++ Games/pong2/Enemy.java
@@ -8,18 +8,21 @@ public class Enemy {
 	//ATRIBUTOS
 	public double x, y;
 	public int wid, hei;
+	public int count_enemy;
 	
 	//METODO CONSTRUTOR
-	public Enemy(double x, double y) {
+	public Enemy(double x, double y, int count_enemy) {
 		this.x = x;
 		this.y = y;
 		this.wid = 40;
 		this.hei = 5;
+		this.count_enemy = count_enemy;
 	}
 	
 	public void tick() {
 		
 		x += (Game.ball.x - x) * 0.2;
+		
 	}
 	
 	public void render(Graphics gph) {
