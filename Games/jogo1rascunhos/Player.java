6e34163a09f0f54e132fdbf9e9f4f7df8662e62c
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2018-10-17 22:15:07 -0300

    Java

---
+++ Games/jogo1rascunhos/Player.java
@@ -0,0 +1,11 @@
+package com.poogametopview.entities;
+
+import java.awt.image.BufferedImage;
+
+public class Player extends Entity{
+
+	public Player(int x, int y, int wid, int hei, BufferedImage sprite) {
+		super(x, y, wid, hei, sprite);
+		
+	}
+}
