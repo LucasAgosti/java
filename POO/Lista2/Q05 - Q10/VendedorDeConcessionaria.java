369d0b447e063b2433980b1791186ca08fe4bddc
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2018-10-23 13:18:18 -0300

    Java

---
+++ POO/Lista2/Q05 - Q10/VendedorDeConcessionaria.java
@@ -0,0 +1,56 @@
+public class VendedorDeConcessionaria extends Funcionario{
+
+	//ATRIBUTOS
+	private String cargo;
+	private double salario;
+	
+	//METODOS SET E GET
+	public void setCargo(String cargo){
+		this.cargo = cargo;
+	}
+	public String getCargo(){
+		return cargo;
+	}
+
+	public void setSalario(double salario){
+		this.salario = salario;
+	}
+	public double getSalario(){
+		return salario;
+	}
+	
+	//CONSTRUTORES 
+	public Funcionario(String nome, String cpf, String dataNascimento, String logradouro, double numero, String bairro, String cidade, String UF, String cargo, double salario){
+		
+		super(nome, cpf, dataNascimento, logradouro, numero, bairro, cidade, UF);
+		this.cargo = cargo;
+		this.salario = salario;
+	}
+	
+	public void vender(double valor){
+		
+		double valorGanho = valor * 0.1;
+		System.out.println("O valor de venda foi: R$" + valor);
+		System.out.println("O funcionário ganhou: R$" + valorGanho);	
+	}
+	
+	//METODO TOSTRING
+	public String toString(){
+	
+		String str = "";
+		str += "*------------------------------------------------------*\n";
+		str += "FUNCIONÁRIO: " + this.nome +  "\n";
+		str += "CPF: " +  this.cpf +  "\n";
+		str += "Data de nascimento: " + this.dataNascimento + "\n";
+		str += "Logradouro: " + this.logradouro + "\n";
+		str += "Número: " + this.numero + "\n";
+		str += "Bairro: " + this.bairro + "\n";
+		str += "Cidade: " + this.cidade + "\n";
+		str += "UF: " + this.UF + "\n";
+		str += "Cargo: " + this.cargo + "\n";
+		str += "Salario: R$ " + this.salario + "\n";
+		str += "*------------------------------------------------------*\n";
+	
+		return str;
+	}
+}
