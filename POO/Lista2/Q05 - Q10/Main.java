5468080aaa7a86ebb2ca5cf01523b326523e19fc
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2018-10-26 14:47:42 -0300

    Java

--- POO/Lista2/Q05 - Q10/Main.java
+++ POO/Lista2/Q05 - Q10/Main.java
@@ -3,9 +3,7 @@ import static java.lang.System.out;
 import java.util.List;
 import java.util.ArrayList;
 
-
-public class UsandoCarro{
-
+public class Main{
 
 	public static int count_veiculos = 0;
 	public static Funcionario func;
@@ -40,7 +38,7 @@ public class UsandoCarro{
 					break;
 					
 				case '4':
-					out.println("Existem " + count_veiculos + " carros\n");
+					out.println("\nQuantidade de carros: " + count_veiculos + "\n");
 					break;
 					
 				case '5':
@@ -52,11 +50,6 @@ public class UsandoCarro{
 					criaFuncionario();
 					out.println();
 					break;
-					
-				case '7':
-					func.vender();
-					out.println();
-					break;
 				
 				default:
 					out.println("Opção não reconhecida");
@@ -66,7 +59,7 @@ public class UsandoCarro{
 			}
 			out.println("*--------------------------------*");
 			out.println("Insira uma opção");
-			out.println("1- Criar pessoa\n2- Criar carro\n3- Criar carro de concessionaria\n4-Verificar a quantidade de carros\nS- Sair");
+			out.println(" 1- Criar pessoa\n 2- Criar carro\n 3- Criar carro de concessionaria\n 4-Verificar a quantidade de carros\n 5- Criar concessionaria\n 6- Criar funcionario\n S- Sair");
 			out.println("*--------------------------------*");
 			
 			op = input.next().charAt(0);
@@ -104,6 +97,42 @@ public class UsandoCarro{
 		Carro carros = new Carro(marc, model, ano, chassi, prop);
 		out.println(carros);
   		count_veiculos++;
+  		
+  		out.println("*--------------------------------*");
+  		out.println("Escolha uma opção:\n\t1- Abastecer\n\t2- Acelerar\n\t3- Frear\n\tS - Sair");
+  		out.println("*--------------------------------*");
+  		
+  		char op = input.next().charAt(0);;
+		
+		while(op != 'S'){
+			switch(op){
+		
+				case '1':
+					out.println("Insira o valor para ser abastecido:");
+					double valor1 = input.nextDouble();	
+					carros.abastecer(valor1);
+					break;
+				
+				case '2':
+					out.println("Insira o valor para acelerar:");
+					double valor2 = input.nextDouble();
+					carros.acelerar(valor2);
+					break;
+	
+				case '3':
+					out.println("Insira o valor para frear:");
+					double valor3 = input.nextDouble();
+					carros.frear(valor3);
+					break;
+				default:
+					out.println("Opção não reconhecida");
+					break;
+			}
+			out.println("*--------------------------------*");
+			out.println("Escolha uma opção:\n\t1- Abastecer\n\t2- Acelerar\n\t3- Frear\n\tS - Sair\n");
+			out.println("*--------------------------------*");
+			op = input.next().charAt(0);
+		}
 	}
 	
 	public static void criaCarroC(){
@@ -152,11 +181,34 @@ public class UsandoCarro{
 	
 		Scanner input = new Scanner(System.in);
 		
-		out.println("Insira dos dados do funcionario");
+		out.println("Insira os dados do funcionario");
 		
-		String
-	
-	
-	
+		String name = input.nextLine();
+		String cpf = input.nextLine();
+		String dataNasc = input.nextLine(); 
+		String carg = input.nextLine();
+		input.nextLine();
+		double salary = input.nextDouble();
+		
+		VendedorDeConcessionaria vendedor = new VendedorDeConcessionaria(name, cpf, dataNasc, carg, salary);
+		out.println(vendedor);
+		
+		out.println("Deseja vender ?\n 1- Sim\n 2- Não");
+		char op = input.next().charAt(0);;
+		
+		switch(op){
+			case '1': 
+				out.println("Insira o valor do veículo a ser vendido");
+				double valor = input.nextDouble();
+				vendedor.vender(valor);
+				break;
+				
+			case '2':
+				break;
+			
+			default:
+				out.println("Opcao nao reconhecida");
+				break;	
+		}
 	}
 }
