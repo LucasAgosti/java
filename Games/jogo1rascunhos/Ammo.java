5b4c2b4c78033f7f43e4c74826521e7e7865ae3d
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2018-11-23 06:44:19 -0300

    Java Entities

---
+++ Games/jogo1rascunhos/Ammo.java
@@ -0,0 +1,11 @@
+package com.poogametopview.entities;
+
+import java.awt.image.BufferedImage;
+
+public class Ammo extends Entity{
+
+	public Ammo(int x, int y, int wid, int hei, BufferedImage sprite) {
+		super(x, y, wid, hei, sprite);
+		
+	}
+}
