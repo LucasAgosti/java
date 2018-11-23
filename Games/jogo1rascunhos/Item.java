2679dc6503befcf1275c4475a2bd8b3341b37c46
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2018-11-23 06:45:59 -0300

    Java Entities

---
+++ Games/jogo1rascunhos/Item.java
@@ -0,0 +1,11 @@
+package com.poogametopview.entities;
+
+import java.awt.image.BufferedImage;
+
+public class Item extends Entity{
+
+	public Item(int x, int y, int wid, int hei, BufferedImage sprite) {
+		super(x, y, wid, hei, sprite);
+		
+	}
+}
