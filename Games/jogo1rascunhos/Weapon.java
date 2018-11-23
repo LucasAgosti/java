3b6f18c9b5c9fcdf0a77235722570cbe3bd9668f
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2018-11-23 06:47:43 -0300

    Java Entities

---
+++ Games/jogo1rascunhos/Weapon.java
@@ -0,0 +1,11 @@
+package com.poogametopview.entities;
+
+import java.awt.image.BufferedImage;
+
+public class Weapon extends Entity {
+
+	public Weapon(int x, int y, int wid, int hei, BufferedImage sprite) {
+		super(x, y, wid, hei, sprite);
+		
+	}
+}
